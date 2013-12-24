215c11eab756cceb7caebc41e510d86478e33d56
aris.goico@gmail.com <aris.goico@gmail.com@e6d01e54-4d44-145b-04ef-93cca208a01b>
2013-12-24 16:45:34 +0000

    Movimiento y prefabs.

---
+++ trunk/Assets/_Scripts/ShipFire.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipFire : MonoBehaviour {
+
+
+	private GameObject target;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
