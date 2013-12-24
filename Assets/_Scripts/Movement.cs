cb1fd6202c551df85f79dac834f4278c43a6c047
aris.goico@gmail.com <aris.goico@gmail.com>
2013-12-24 16:45:34 +0000

    Movimiento y prefabs.

---
+++ Assets/_Scripts/Movement.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Movement : MonoBehaviour {
+
+	public GameObject initShip;
+	private GameObject ship;
+	public GameObject initWeapon;
+	private GameObject weapon;
+	public GameObject initSkill;
+	private GameObject skill;
+	private ShipValues shipValues;
+	private GameObject cam;
+	private Vector3 camInitPos;
+	private Transform placeholderShip;
+
+	// Use this for initialization
+	void Start() {
+		if (initShip == null) {
+			Debug.LogError("No initial ship was assigned to Player " + this.name + ". No controls initialized.");
+			this.enabled = false;
+		}
+
+		foreach (Transform child in transform) {
+			if (child.tag == "MainCamera") {
+				cam = child.gameObject;
+				camInitPos = cam.transform.position;
+			}
+			if (child.tag == "PlaceholderShip") {
+				placeholderShip = child;
+			}
+		}
+		changeShip(initShip);
+//		GameObject temp = Instantiate(initShip, placeholderShip.position, placeholderShip.rotation) as GameObject;
+//		ship = temp;
+//		ship.transform.parent = this.transform;
+//		shipValues = ship.GetComponent<ShipValues>();
+		changeWeapon(initWeapon);
+//		temp = Instantiate(initWeapon, shipValues.placeholderWeapon.position, shipValues.placeholderWeapon.rotation) as GameObject;
+//		weapon = temp;
+//		weapon.transform.parent = ship.transform;
+	}
+	
+	// Movement
+	void Update() {
+		Vector3 moveDir = Vector3.zero;
+		moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		if (moveDir.magnitude > 0f) {
+			ship.transform.rotation = Quaternion.RotateTowards(ship.transform.rotation, Quaternion.LookRotation(moveDir, Vector3.up), shipValues.angularSpeed);
+		}
+		ship.rigidbody.AddForce(moveDir.magnitude * ship.transform.forward * shipValues.shipSpeed);
+		cam.transform.position = camInitPos + ship.transform.position;
+	}
+
+	public void changeShip(GameObject newShip) {
+		GameObject.Destroy(ship);
+		GameObject temp = Instantiate(newShip, placeholderShip.position, placeholderShip.rotation) as GameObject;
+		ship = temp;
+		ship.transform.parent = this.transform;
+		shipValues = ship.GetComponent<ShipValues>();
+	}
+
+	public void changeWeapon(GameObject newWeapon) {
+		GameObject.Destroy(weapon);
+		GameObject temp = Instantiate(newWeapon, shipValues.placeholderWeapon.position, shipValues.placeholderWeapon.rotation) as GameObject;
+		weapon = temp;
+		weapon.transform.parent = ship.transform;
+	}
+}
---
+++ Assets/_Scripts/ShipValues.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ShipValues : MonoBehaviour {
+
+	public float shipDrag = 1f;
+	public float shipSpeed = 1f;
+	public float shipMass = 1f;
+	public float angularSpeed = 1f;
+	public Transform placeholderWeapon;
+	public Transform placeholderSkill;
+
+	void Awake() {
+		this.rigidbody.mass = shipMass;
+		this.rigidbody.drag = shipDrag;
+		foreach (Transform child in transform) {
+			if (child.tag == "PlaceholderSkill") {
+				placeholderSkill = child;
+			}
+			if (child.tag == "PlaceholderWeapon") {
+				placeholderWeapon = child;
+			}
+		}
+	}
+}
