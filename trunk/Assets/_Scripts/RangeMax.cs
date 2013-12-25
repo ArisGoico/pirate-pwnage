1983893e1d642edef997c3db31cd342278f6a394
aris.goico@gmail.com <aris.goico@gmail.com@e6d01e54-4d44-145b-04ef-93cca208a01b>
2013-12-25 23:25:17 +0000

    Sistema de disparo funcionando (a excepción de las balas, que hay que darles parámetros como el daño o la dirección de movimiento aun).

---
+++ trunk/Assets/_Scripts/RangeMax.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class RangeMax : MonoBehaviour {
+
+	private Transform weapon;
+	private ShipWeapon weaponScript;
+
+	// Use this for initialization
+	void Awake() {
+		searchWeapon();
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.transform.tag == "Ship") {
+			if (weaponScript == null) {
+				searchWeapon();
+			}
+			if (weaponScript != null) {
+				weaponScript.addTarget(other.gameObject);
+//				Debug.Log("Target " + other.gameObject.name + " added = " + temp + ".");
+			}
+			else {
+				Debug.LogError("The ship " + this.transform.parent.name + " has no weapon systems.");
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.transform.tag == "Ship") {
+			if (weaponScript == null) {
+				searchWeapon();
+			}
+			if (weaponScript != null) {
+				weaponScript.delTarget(other.gameObject);
+			}
+			else {
+				Debug.LogError("The ship " + this.transform.name + " has no weapon systems.");
+			}
+		}
+	}
+
+	private void searchWeapon() {
+		foreach (Transform child in transform.parent) {
+			if (child.tag == "Weapon") {
+				weapon = child;
+				weaponScript = weapon.GetComponent<ShipWeapon>();
+			}
+		}
+	}
+}
--- trunk/Assets/_Scripts/ShipWeapon.cs
+++ trunk/Assets/_Scripts/ShipWeapon.cs
@@ -1,24 +1,90 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class ShipWeapon : MonoBehaviour {
 
+	public GameObject bullet;
+	public int damagePerHit = 1;
+	public float hitsPerSecond = 1f;
+	private float lastShot = 0f;
+	private bool shooting = false;
 
+	private GameObject[] targets;
+	private int targetNum = 0;
 
-	private GameObject target;
-
+	private Transform placeHolderBullet;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start() {
+		targets = new GameObject[10];
+		if (bullet == null) {
+			Debug.LogError("The ship " + this.transform.parent.name + " has no bullets defined. Deactivating weapons.");
+			this.enabled = false;
+		}
+		foreach (Transform child in transform) {
+			if (child.tag == "PlaceholderBullet") {
+				placeHolderBullet = child;
+			}
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update() {
+		if (targets[0] != null) {
+			Vector3 temp = targets[0].transform.position - this.transform.position;
+			this.transform.rotation = Quaternion.LookRotation(new Vector3(temp.x, 0f, temp.z));
+			if (!shooting) {
+				InvokeRepeating("shootBullet", 0.1f, 1f / hitsPerSecond);
+				shooting = true;
+			}
+		}
+		else {
+			this.transform.rotation = Quaternion.LookRotation(this.transform.parent.forward);
+			CancelInvoke("shootBullet");
+			shooting = false;
+		}
 	}
 
-	void OnColliderEnter() {
+	public bool addTarget(GameObject newTarget) {
+		if (newTarget.tag == "Ship" && targetNum < targets.Length && !checkArray(newTarget, targets, targetNum)) {
+			targets[targetNum] = newTarget;
+			targetNum ++;
+			return true;
+		}		
+		return false;
+	}
 
+	public bool delTarget(GameObject newTarget) {
+		bool found = false;
+		int tempIndex = -1;
+		for (int i = 0; i < targets.Length; i++) {
+			if (targets[i] == newTarget) {
+				found = true;
+				targets[i] = null;
+				tempIndex = i;
+			}
+		}
+		if (found) {
+			for (int i = tempIndex; i < targetNum - 1; i++) {
+				targets[i] = targets[i + 1];
+			}
+			targets[targetNum - 1] = null;
+			targetNum--;
+		}
+		return found;
+	}
+
+	private bool checkArray(GameObject obj, GameObject[] array, int arrayIndex) {
+		for (int i = 0; i < arrayIndex; i++) {
+			if (array[i] == obj) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private void shootBullet() {
+		Instantiate(bullet, placeHolderBullet.position, placeHolderBullet.rotation);
 	}
 }
